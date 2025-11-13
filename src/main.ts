c2d65d8f3002959d659d2a1ce10bbad255848561
OSpoon <zxin088@gmail.com>
2025-11-14 00:05:09 +0800

    feat: add TypeScript configuration files and Vite setup
    - Created tsconfig.app.json for application-specific TypeScript settings.
    - Added tsconfig.json to manage project references.
    - Introduced tsconfig.node.json for Node.js specific TypeScript configurations.
    - Implemented vite.config.ts for Vite build tool configuration with Vue, JSX, and Tailwind CSS support.

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Aura from '@primeuix/themes/aura'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import PrimeVue from 'primevue/config'
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
+import App from './App.vue'
+import router from './router'
+import '@/assets/tailwind.css'
+import '@/assets/styles.scss'
+
+const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.app-dark',
+    },
+  },
+})
+app.use(ToastService)
+app.use(ConfirmationService)
+
+app.mount('#app')
